056b728f14de8a398f7438f04cd52cc6d251a07b
Azizul Hoque <44068019+AzizulBSc@users.noreply.github.com>
2022-08-13 01:21:42 +0600

    Create Day 19: Interfaces.cpp

---
+++ Day 19: Interfaces.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+public:
+    int divisorSum(int n) {
+        if(n==1) return 1;
+        int divisorSum = 1+n;
+        for(int i = 2;i<n/2+1;i++){
+            if(n%i==0){
+                divisorSum += i;
+            }
+        }
+        return divisorSum;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
